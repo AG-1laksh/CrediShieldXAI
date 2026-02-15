e5b44bb74ccb23adceaf980f9848e51a4d0095c1
AG-1laksh <resilient.warrior123@gmail.com>
2026-02-15 13:52:40 +0530

    backend_fix

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
+    allow_origin_regex=".*",
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
